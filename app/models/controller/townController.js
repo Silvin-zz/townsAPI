3eba21f79ec2d5f3956ae9c096d61b364b644385
Silvin <bravocado@gmail.com>
2015-09-17 16:42:36 -0500

    integrando update a traves de put

--- app/models/controller/townController.js
+++ app/models/controller/townController.js
@@ -52,11 +52,7 @@ router.put('/:id', function(req, res){
         if(err){
             res.send(err);
         }
-
-        console.log("=============ENTRANDO =============================");
-        console.log(req.body);
-
-        if(typeof(req.body.name) !== undefined){  console.log("entramos ===================");   townObject.name             = req.body.name;         }
+        if("name"        in req.body){     townObject.name             = req.body.name;         }
         if("description" in req.body){     townObject.description      = req.body.description;  }
         if("state"       in req.body){     townObject.state            = req.body.state;        }
         if("food"        in req.body){     townObject.food             = req.body.food;         }
